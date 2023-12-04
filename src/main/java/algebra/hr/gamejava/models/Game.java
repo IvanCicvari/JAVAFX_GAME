fe101832e902ec66992946c0fa025e70638d4386
IvanCicvaricGL <145016156+IvanCicvaricGL@users.noreply.github.com>
2023-12-04 19:11:33 +0100

    developing

--- src/main/java/algebra/hr/gamejava/models/Game.java
+++ src/main/java/algebra/hr/gamejava/models/Game.java
@@ -1,5 +1,7 @@
 package algebra.hr.gamejava.models;
 
+import javafx.scene.image.ImageView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -45,5 +47,80 @@ public class Game {
             }
         }
         stockPile.add(card);
+    }
+
+    public Card getTopCard(){
+        return new Card(validValueOfCard,validConutry);
+    }
+    public ImageView getTopCardImage(){
+        return new ImageView(validValueOfCard+"-"+validConutry+".png");
+    }
+    /*
+    public boolean isGameOver(){
+        for (String player:this.playerID){
+            if (hasEmptyHand(player)){
+                return true;
+            }
+        }
+        return false;
+    }*/ //new logic needs to bee
+    public String getCurrentPlayer(){
+        return this.playerID[this.currentPlayer];
+    }
+    public String getPreviousPlayer(int i){
+        int index = this.currentPlayer-i;
+        if (index == -1)
+        {
+            index =playerID.length-1;
+        }
+        return this.playerID[index];
+    }
+    public String[] getPlayers(){
+        return playerID;
+    }
+    public ArrayList<Card>getPlayerHand(String pid){
+        int index =Arrays.asList(playerID).indexOf(pid);
+        return playerHand.get(index);
+    }
+
+    public int getPlayerHandSize(String pid)
+    {
+        return getPlayerHand(pid).size();
+    }
+    public Card getPlayerCard(String pid,int choice){
+        ArrayList<Card> hand = getPlayerHand(pid);
+        return hand.get(choice);
+    }
+
+    public boolean hasEmptyHand(String pid){
+        return getPlayerHand(pid).isEmpty();
+    }
+    public boolean validCardPlay(Card card){
+        return card.getValueOfCard() == validValueOfCard || card.getCountry() == validConutry;
+    }
+    public void checkPlayerTurn(String pid) throws InvalidPlayerTurnException{
+        if (this.playerID[this.currentPlayer] != pid){
+            throw new InvalidPlayerTurnException("It is not" +pid+" 's turn",pid);
+        }
+    }
+    public void  submitDraw(String pid) throws InvalidPlayerTurnException{
+        if (deck.isEmpty()){
+            deck.replaceDeckWith(stockPile);
+            deck.shuffle();
+        }
+        getPlayerHand(pid).add(deck.drawCard());
+        if (!gameDirection){
+            currentPlayer = (currentPlayer+1)%playerID.length;
+        } else if (gameDirection) {
+            currentPlayer = (currentPlayer-1)%playerID.length;
+            if (currentPlayer ==-1)
+            {
+                currentPlayer =playerID.length -1;
+            }
+
+        }
+    }
+    public void setCardValue (Card.ValueOfCard cardValue){
+        validValueOfCard = cardValue;
     }
 }
